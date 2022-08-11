fcf9fc7490a66487d9a5d253bba9d12b22a1b6fb
fummyolalekan <fummyolalekan@gmail.com>
2022-08-10 18:00:08 -0700

    hot fix

--- liststr.c
+++ liststr.c
@@ -56,12 +56,12 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 	new_node->num = num;
 	if (str)
 	{
-		 new_node->str = _strdup(str);
-		 if (!new_node->str)
-		 {
-			 free(new_node);
-			 return (NULL);
-		 }
+		new_node->str = _strdup(str);
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
 	}
 	if (node)
 	{
